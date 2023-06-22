6db42a06103f08d5441ae904070ad9ceb1334724
Soumyajit <soumyajitchatterjee380@gmail.com>
2023-06-21 22:02:05 -0700

    changed msg

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World i am Soumyajit chatterjee and thsi should work ");
+		System.out.println("Hello Word i am Soumyajit chatterjee and thsi should work ");
 	}
 }
