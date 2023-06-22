e70724da0cc98a7334d0ac799636b00d056885b9
Soumyajit <soumyajitchatterjee380@gmail.com>
2023-06-21 21:10:35 -0700

    Edited Hello World Agin again

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World i am Soumyajit chatterjee are u ok! plz work I am happy it works!!");
+		System.out.println("Hello World i am Soumyajit chatterjee ");
 	}
 }
